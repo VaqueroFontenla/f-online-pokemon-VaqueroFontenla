4e16ac5460c450fb2b0f39e4632ac1029559b36c
VaqueroFontenla <loret.vaquero@gmail.com>
2018-10-05 12:11:32 +0200

    call API and LocalStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,27 @@ import PokerList from '../PokerList/PokerList.js';
 import './App.css';
 
 class App extends Component {
+
+
+  componentDidMount() {
+    const pokemons = [];
+    const url = 'https://pokeapi.co/api/v2/pokemon/';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem('data', JSON.stringify(data));
+
+      for (const pokemon of data.results) {
+        fetch(pokemon.url)
+        .then(res => res.json())
+        .then(datapokemon => {
+          localStorage.setItem('datapokemon', JSON.stringify(datapokemon));
+          console.log(datapokemon)
+        });
+          }
+  })
+}
+
   render() {
     return (
       <div className="App">
